ef8aab874b847c6ea24a4ba36d23b893b2b10079
Om Kanabar <206570655+om-kanabar@users.noreply.github.com>
2026-09-23 10:10:13 -0500

    Add select samples to python code

--- mosaic.py
+++ mosaic.py
@@ -1,15 +1,14 @@
 # (c) Om Kanabar 2026
 
-import rich
-import Pytorch
+# import rich
+# import Pytorch
+import random as rand
 
 def _plausability(z):
     pass
 
 def _update_sets(D, U, i, j, z=True):
-    if D and U and i and j:
-        pass
-    else:
+    if not D or not U or not i or not j:
         return
 
     pair = frozenset({i, j})
@@ -21,23 +20,40 @@ def _update_sets(D, U, i, j, z=True):
 
     return
 
+
 def _select_samples(D, U, attempts=0, max_attempts=1000):
-    pass
+    if attempts > max_attempts:
+        return None, None
+
+    i = rand.sample(D, 1)[0]
+    j = rand.sample(D - {i}, 1)[0]
+
+    if frozenset({i,j}) in U:
+        return _select_samples(D, U, attempts+1, max_attempts)
+
+    _update_sets(D, U, i, j)
+    return i, j
+
 
 def _initial_comparison(D, U, i, j):
     pass
 
+
 def _crossing_point(i,j):
     pass
+
 
 def _combine(shift, i , j):
     pass
 
+
 def Mosaic(D):
     pass
 
+
 def _synthesize_sample(D, U, D_prime, max_retries=50, retries=0):
     pass
+
 
 def synthesizeSingleSample(D):
     pass
